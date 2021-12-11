943b1a4f0197780e2c674e201d5babcbe0ad2927
AyyanFawad <77777333+AyyanFawad@users.noreply.github.com>
2021-12-11 21:27:15 +0500

    Add files via upload

---
+++ GameObject.hpp
@@ -0,0 +1,30 @@
+#pragma once
+//#include "Game.hpp"
+#include "SDL.h"
+#include "SDL_image.h"
+#include <stdio.h>
+#include "Map.hpp"
+
+class GameObject : public Game
+{
+public:
+    GameObject(const char *texturesheet, int x, int y);
+    ~GameObject();
+    void Update();
+    void heartUpdate();
+    void livesUpdate();
+    void Render();
+    void move();
+    void moveBallDown();
+    // SDL_Rect brick_coords[500];
+    // static void brickCoords(SDL_Rect brickDest, int i);
+
+private:
+    int ver_move;
+    int hor_move;
+    int lives_xpos, lives_ypos;
+    int xpos;
+    int ypos;
+    SDL_Texture *objTexture;
+    SDL_Rect srcRect, destRect;
+};
